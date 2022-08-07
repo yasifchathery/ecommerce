aa69d079482f599b5b00ffa31441acfdff1af5fd
yasifchathery <yasif6789@gmail.com>
2022-08-07 02:37:31 -0700

    first commit

---
+++ ecommerce/shopapp/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import product,category
+
+# Register your models here.
+
+
+class categoryadmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(category,categoryadmin)
+
+class productadmin(admin.ModelAdmin):
+    list_display = ['name','slug','price','available','update','created']
+    list_editable = ['price','available','slug']
+    list_per_page = 20
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(product,productadmin)
